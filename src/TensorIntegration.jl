13d05584d77811571f095cc4fbdbaf4453a9cb73
christoph hedtrich <christophhedtrich@gmail.com>
2019-06-04 17:23:31 +0200

    add

--- src/TensorIntegration.jl
+++ src/TensorIntegration.jl
@@ -1,5 +1,84 @@
 module TensorIntegration
+    export make_tensor, tensor_integrate
+    
+    function simpson(a,b,N)
+        if mod(N,2) == 0
+            N += 1
+        end
+        if N > 1
+            xs = collect(range(a, b, length=N))
+            ws = ones(N)
+            ws[2:2:end-1].=4
+            ws[3:2:end-2].=2
+            ws = ws*(xs[2]-xs[1])/3.
+        else
+            xs = [(a+b)/2.]
+            ws = [1.] 
+        end
+        return xs, ws
+    end
 
-greet() = print("Hello World!")
+    function tensor_simpson(a,b,N; lowprecision = false)
+        if lowprecision
+            numbertype = Float32
+        else
+            numbertype = Float64
+        end
+        d = length(N)
+        for i = 1:d
+            if mod(N[i],2) == 0
+                N[i] += 1
+            end
+        end
 
-end # module
+        xs = Array{Vector{numbertype}}(d)
+        for i in 1:d
+            if N[i] > 1
+                xs[i] = collect(range(a[i], b[i], length = N[i]))
+            else
+                xs[i] = [(a[i]+b[i])/2.]
+            end
+        end
+        ws = Array{Vector{numbertype}}(d)
+        for i in 1:d
+            if N[i] > 1
+                ws[i] = ones(N[i])
+                ws[i][2:2:end-1]=4
+                ws[i][3:2:end-2]=2
+                ws[i] = ws[i]*(xs[i][2]-xs[i][1])/3.
+            else
+                ws[i] = [1.]
+            end
+        end
+        xmat, w = tensor_integrate(xs, ws, N)
+        return xmat, w
+    end
+
+    function tensor_integrate(xs, ws, N)
+        totN = prod(N)
+        d = length(N)
+        xmat = Array{eltype(xs[1])}(totN,d)
+        wmat = Array{eltype(ws[1])}(totN,d)
+        for i in 1:d
+            repout = Int(totN/prod(N[1:i]))
+            repin = Int(totN/N[i]/repout)
+            xmat[:, i] = repeat(xs[i], inner=repin, outer=repout)
+            wmat[:, i] = repeat(ws[i], inner=repin, outer=repout)
+        end
+        w = prod(wmat, 2)
+        return xmat, w
+    end
+
+    function make_tensor(xs, N)
+        totN = prod(N)
+        d = length(N)
+        xmat = Array{eltype(xs[1])}(totN,d)
+        for i in 1:d
+            repout = Int(totN/prod(N[1:i]))
+            repin = Int(totN/N[i]/repout)
+            xmat[:, i] = repeat(xs[i], inner=repin, outer=repout)
+        end
+        return xmat
+    end
+end
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,59 @@
 using TensorIntegration
 using Test
 
-@testset "TensorIntegration.jl" begin
-    # Write your own tests here.
-end
+
+# add test for basic integrals
+N = 5
+# uniform expectation 
+a = 1. 
+b = 3. 
+pdf(x, a, b) = 1./(b-a)
+grid, weights = TensorIntegration.simpson(a, b, N)
+
+Exp = (grid.*pdf.(grid, a, b))'*weights
+@test Exp == (a+b)/2.
+
+
+# now in 2d
+N2d = [5, 5]
+a2d = [1., 1.]
+b2d = [2., 2.]
+pdf(x, a, b, d) = 1/(b[d]-a[d])
+
+grid, weights = TensorIntegration.tensor_simpson(a2d, b2d, N2d)
+Exp1 = (grid[:,1].*pdf(grid, a2d, b2d, 1))'*weights
+Exp2 = (grid[:,2].*pdf(grid, a2d, b2d, 2))'*weights
+
+@test Exp1[1] == (a2d[1]+b2d[1])/2.
+@test Exp2[1] == (a2d[2]+b2d[2])/2.
+
+
+# now in 3d
+N3d = [5, 5, 5]
+a3d = [1., 1., 0.1]
+b3d = [2., 2., 3.3]
+pdf(x, a::Array, b::Array) = 1./prod(b3d.-a3d)
+
+grid, weights = TensorIntegration.tensor_simpson(a3d, b3d, N3d)
+Exp1 = (grid[:,1].*pdf(grid, a3d, b3d))'*weights
+Exp2 = (grid[:,2].*pdf(grid, a3d, b3d))'*weights
+Exp3 = (grid[:,3].*pdf(grid, a3d, b3d))'*weights
+
+@test isapprox(Exp1[1], (a3d[1]+b3d[1])/2.)
+@test isapprox(Exp2[1], (a3d[2]+b3d[2])/2.)
+@test isapprox(Exp3[1], (a3d[3]+b3d[3])/2.)
+
+# now with one dimension only 1 point 
+N3d = [5, 5, 1]
+a3d = [1., 1., 1.]
+b3d = [2., 2., 2.]
+pdf(x, a::Array, b::Array) = 1./prod(b3d.-a3d)
+
+grid, weights = TensorIntegration.tensor_simpson(a3d, b3d, N3d)
+Exp1 = (grid[:,1].*pdf(grid, a3d, b3d))'*weights
+Exp2 = (grid[:,2].*pdf(grid, a3d, b3d))'*weights
+Exp3 = (grid[:,3].*pdf(grid, a3d, b3d))'*weights
+
+@test isapprox(Exp1[1], (a3d[1]+b3d[1])/2.)
+@test isapprox(Exp2[1], (a3d[2]+b3d[2])/2.)
+@test isapprox(Exp3[1], (a3d[3]+b3d[3])/2.)
